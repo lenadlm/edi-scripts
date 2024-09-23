3c1982fa0c66de75f1993a888c70029f7134abd0
Leonard Mbelle <58833407+lenadlm@users.noreply.github.com>
2024-09-23 19:52:53 +0300

    Update edo.ps1

--- edo.ps1
+++ edo.ps1
@@ -21,26 +21,35 @@ do {
 
 Write-Host "Replacing '$oldChar' with '$newChar' in filenames"
 
+# Array of string replacements in the format: OldString -> NewString
+$replacements = @(
+    @('OldString', 'NewString'),
+    @('2431S ', '02431N'),
+    @('02SHXS1MA', '02SHYN1MA'),
+    @('02SGPS1MA', '02SGQN1MA'),
+    @(' 431W     ', ' KKRM0431W'),
+    @('02SHRS1MA', '02SHSN1MA'),
+    @('02SHZS1MA', '02SI0N1MA'),
+    @(' 432W     ', ' KLMB0432W'),
+    @('02SIHS1MA', '02SIIN1MA'),
+    @(' 433W     ', ' KKAY0433W'),
+    @('02SSIS1MA', '02STGN1MA'),
+    @(' 435W     ', ' KKMI0435W'),
+    @('OldString3', 'NewString3')
+)
+
 # Get all .txt files in the directory
 $files = Get-ChildItem "$directoryPath\*.txt"
 
 foreach ($file in $files) {
-    # Replace specific strings in the file content
+    # Get file content
     $content = Get-Content $file.FullName
-    $content = $content -replace 'OldString', 'NewString'
-    $content = $content -replace '2431S ', '02431N'
-    $content = $content -replace '02SHXS1MA', '02SHYN1MA'
-    $content = $content -replace '02SGPS1MA', '02SGQN1MA'
-    $content = $content -replace ' 431W     ', ' KKRM0431W'
-    $content = $content -replace '02SHRS1MA', '02SHSN1MA'
-    $content = $content -replace '02SHZS1MA', '02SI0N1MA'
-    $content = $content -replace ' 432W     ', ' KLMB0432W'
-    $content = $content -replace '02SIHS1MA', '02SIIN1MA'
-    $content = $content -replace ' 433W     ', ' KKAY0433W'
-    $content = $content -replace '02SSIS1MA', '02STGN1MA'
-    $content = $content -replace ' 435W     ', ' KKMI0435W'
-    $content = $content -replace 'OldString3', 'NewString3'
-
+    
+    # Apply each replacement
+    foreach ($replacement in $replacements) {
+        $content = $content -replace $replacement[0], $replacement[1]
+    }
+    
     # Write the modified content back to the file
     $content | Set-Content -Encoding UTF8 $file.FullName
 
