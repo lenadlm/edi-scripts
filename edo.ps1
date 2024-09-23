a3e890428fa98cc10096c1842614b355790cb293
Leonard Mbelle <58833407+lenadlm@users.noreply.github.com>
2024-09-23 16:29:56 +0300

    Update edo.ps1

--- edo.ps1
+++ edo.ps1
@@ -27,35 +27,34 @@ $files = Get-ChildItem "$directoryPath\*.txt"
 foreach ($file in $files) {
     # Replace specific strings in the file content
     $content = Get-Content $file.FullName
-    $content = $content `
-        -replace 'OldString', 'NewString' `
-        -replace '2431S ', '02431N' `
-        -replace '02SHXS1MA', '02SHYN1MA' `
-        -replace '02SGPS1MA', '02SGQN1MA' `
-        -replace ' 431W     ', ' KKRM0431W' `
-        -replace '02SHRS1MA', '02SHSN1MA' `
-        -replace '02SHZS1MA', '02SI0N1MA' `
-        -replace ' 432W     ', ' KLMB0432W' `
-        -replace '02SIHS1MA', '02SIIN1MA' `
-        -replace ' 433W     ', ' KKAY0433W' `
-        -replace 'OldString3', 'NewString3' `
-        -replace 'OldString2', 'NewString2' `
-		-replace 'OldString3', 'NewString3' `
-        -replace 'OldString2', 'NewString2' `
-		-replace 'OldString3', 'NewString3' `
-        -replace 'OldString3', 'NewString3'
+    $content = $content -replace 'OldString', 'NewString'
+    $content = $content -replace '2431S ', '02431N'
+    $content = $content -replace '02SHXS1MA', '02SHYN1MA'
+    $content = $content -replace '02SGPS1MA', '02SGQN1MA'
+    $content = $content -replace ' 431W     ', ' KKRM0431W'
+    $content = $content -replace '02SHRS1MA', '02SHSN1MA'
+    $content = $content -replace '02SHZS1MA', '02SI0N1MA'
+    $content = $content -replace ' 432W     ', ' KLMB0432W'
+    $content = $content -replace '02SIHS1MA', '02SIIN1MA'
+    $content = $content -replace ' 433W     ', ' KKAY0433W'
+    $content = $content -replace '02SSIS1MA', '02STGN1MA'
+    $content = $content -replace ' 435W     ', ' KKMI0435W'
+    $content = $content -replace 'OldString3', 'NewString3'
 
     # Write the modified content back to the file
-    $content | Set-Content -Encoding ASCII $file.FullName
+    $content | Set-Content -Encoding UTF8 $file.FullName
 
     # Check if the filename contains the old character
     if ($file.Name -like "*$oldChar*") {
         # Create the new filename by replacing the old character with the new one
         $newFileName = $file.Name -replace $oldChar, $newChar
 
-        # Rename the file
-        Rename-Item -Path $file.FullName -NewName $newFileName
-        
-        Write-Host "Renamed '$($file.Name)' to '$newFileName'"
+        # Rename the file with error handling
+        try {
+            Rename-Item -Path $file.FullName -NewName $newFileName
+            Write-Host "Renamed '$($file.Name)' to '$newFileName'"
+        } catch {
+            Write-Host "Failed to rename '$($file.Name)': $_"
+        }
     }
 }
